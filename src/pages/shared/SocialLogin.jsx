856076dab1e47b78bcfb822187eaf570c6724bc0
Arafat-Bhuiyan <arafatbhuiyan41@gmail.com>
2025-08-06 16:11:46 +0600

    social login page

--- src/pages/shared/SocialLogin.jsx
+++ src/pages/shared/SocialLogin.jsx
@@ -18,7 +18,7 @@ const SocialLogin = () => {
       >
         <button
           onClick={handleGoogleLogin}
-          className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm hover:shadow-md transition"
+          className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-300 rounded-3xl shadow-sm hover:shadow-md transition"
         >
           <img src={google} alt="Google logo" className="w-5 h-5" />
           <span className="text-gray-700 font-medium">Sign in with Google</span>
